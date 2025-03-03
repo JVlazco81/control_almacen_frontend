a6ff2d92813c318fd8e378024b9ac9b3bb2d117c
CristianConkle <al068342@uacam.mx>
2025-03-03 17:55:24 -0600

    Se añadieron las rutas a los archivos para navegacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:control_almacen_frontend/screens/entradas_art.dart';
+import 'package:control_almacen_frontend/screens/existencias_view.dart';
+import 'package:control_almacen_frontend/screens/historial_view.dart';
+import 'package:control_almacen_frontend/screens/salidas_art.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:control_almacen_frontend/screens/bienvenida.dart';
 
 void main() {
   runApp(
@@ -13,10 +18,23 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Mi Aplicación Web',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      // Definir las rutas para la navegación
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Bienvenida(),
+        '/existencias': (context) => Existencias_View(),
+        '/historial': (context) => Historial_View(),
+        '/entrada': (context) => Entradas_Art(),
+        '/salida': (context) => Salidas_Art(),
+      },
       //home: HomeScreen(),
     );
   }

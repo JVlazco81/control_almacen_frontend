ef02314f11a9cdcf3e878dcd38595bada95a00f2
CristianConkle <al068342@uacam.mx>
2025-03-04 16:54:48 -0600

    PCA-9 Implementacion visual del formulario de salida (no envia datos al back)

--- lib/screens/salidas_art.dart
+++ lib/screens/salidas_art.dart
@@ -1,11 +1,198 @@
 import 'package:flutter/material.dart';
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:number_inc_dec/number_inc_dec.dart';
 import 'package:control_almacen_frontend/widgets/BaseLayout.dart';
 
-class Salidas_Art extends StatelessWidget {
+class Salidas_Art extends StatefulWidget {
   const Salidas_Art({super.key});
 
   @override
+  _Salidas_ArtState createState() => _Salidas_ArtState();
+}
+
+class _Salidas_ArtState extends State<Salidas_Art> {
+  TextEditingController fechaActualController = TextEditingController();
+  TextEditingController cantidadSalidaController = TextEditingController(
+    text: "1",
+  );
+
+  List<String> clasificaciones = [
+    'Clasificación 1',
+    'Clasificación 2',
+    'Clasificación 3',
+    'Clasificación Especial',
+    'Otra Clasificación',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    fechaActualController.text =
+        "${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}";
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return BaseLayout(bodyContent: Center(child: Text('Vista de Salida')));
+    return BaseLayout(
+      bodyContent: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Salida de productos del sistema',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+
+            // Primera tabla (Información del artículo)
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(labelText: 'Folio'),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Nombre del artículo',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: DropdownSearch<String>(
+                          items: clasificaciones,
+                          popupProps: PopupProps.menu(
+                            showSearchBox: true,
+                            searchFieldProps: TextFieldProps(
+                              decoration: InputDecoration(
+                                labelText: "Buscar clasificación...",
+                              ),
+                            ),
+                          ),
+                          dropdownDecoratorProps: DropDownDecoratorProps(
+                            dropdownSearchDecoration: InputDecoration(
+                              labelText: 'Clasificación',
+                            ),
+                          ),
+                          filterFn: (item, filter) {
+                            return item.toLowerCase().contains(
+                              filter.toLowerCase(),
+                            );
+                          },
+                          onChanged: (value) {
+                            print('Seleccionado: $value');
+                          },
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: NumberInputWithIncrementDecrement(
+                          controller: cantidadSalidaController,
+                          min: 1,
+                          max: 1000,
+                          initialValue: 1,
+                          incDecFactor: 1,
+                          numberFieldDecoration: InputDecoration(
+                            labelText: 'Cantidad',
+                            border: OutlineInputBorder(),
+                          ),
+                          widgetContainerDecoration: BoxDecoration(
+                            border: Border.all(color: Colors.transparent),
+                          ),
+                          /*onChanged: (value) {
+                            print('Cantidad seleccionada: $value');
+                          },*/
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            // Segunda tabla (Información de salida)
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Departamento',
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Encargado del departamento',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: fechaActualController,
+                          decoration: InputDecoration(
+                            labelText: 'Fecha actual',
+                          ),
+                          readOnly: true,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            // Tabla "En espera para salir"
+            Container(
+              padding: EdgeInsets.all(10),
+              color: Colors.grey[800],
+              child: Center(
+                child: Text(
+                  'En espera para salir',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

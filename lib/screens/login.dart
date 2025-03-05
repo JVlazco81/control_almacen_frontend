5f05c2ac1bd0e19349f34ab332e53b2c404b00a5
CristianConkle <al068342@uacam.mx>
2025-03-05 15:44:58 -0600

    PCA-28 Se agregó el formulario de login (sin funcionamiento)

---
+++ lib/screens/login.dart
@@ -0,0 +1,102 @@
+import 'package:control_almacen_frontend/screens/bienvenida.dart';
+import 'package:flutter/material.dart';
+import 'package:control_almacen_frontend/screens/bienvenida.dart'; // Asegúrate de que la ruta sea correcta
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isSmallScreen = MediaQuery.of(context).size.width < 600;
+
+    return Scaffold(
+      body: Center(
+        child:
+            isSmallScreen
+                ? Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [_FormContent()],
+                )
+                : Container(
+                  padding: const EdgeInsets.all(32.0),
+                  constraints: const BoxConstraints(maxWidth: 800),
+                  child: Row(
+                    children: const [
+                      Expanded(child: Center(child: FlutterLogo(size: 100))),
+                      Expanded(child: Center(child: _FormContent())),
+                    ],
+                  ),
+                ),
+      ),
+    );
+  }
+}
+
+class _FormContent extends StatefulWidget {
+  const _FormContent({Key? key}) : super(key: key);
+
+  @override
+  State<_FormContent> createState() => __FormContentState();
+}
+
+class __FormContentState extends State<_FormContent> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 300),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Matrícula',
+                hintText: 'Ingrese su matrícula',
+                prefixIcon: Icon(Icons.badge_outlined),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'Contraseña',
+                hintText: 'Ingrese su contraseña',
+                prefixIcon: Icon(Icons.lock_outline_rounded),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    'Iniciar sesión',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Bienvenida()),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

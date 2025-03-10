67f786fae8b4a114fca9d8e8ff40246863d8b844
JVlazco81 <al062648@uacam.mx>
2025-03-10 16:03:30 -0600

    cambios

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -12,33 +12,29 @@ class Login extends StatelessWidget {
 
     return Scaffold(
       body: Center(
-        child:
-            isSmallScreen
-                ? Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: const [_FormContent()],
-                )
-                : Container(
-                  padding: const EdgeInsets.all(70.0),
-                  constraints: const BoxConstraints(maxWidth: 800),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Center(
-                          child: Image.asset(
-                            'assets/images/DropboxLogo.png',
-                            width: 150,
-                            height: 150,
-                            color: Colors.black,
-                            colorBlendMode: BlendMode.srcIn,
-                            fit: BoxFit.contain,
-                          ),
+        child: isSmallScreen
+            ? Column(mainAxisSize: MainAxisSize.min, children: const [_FormContent()])
+            : Container(
+                padding: const EdgeInsets.all(70.0),
+                constraints: const BoxConstraints(maxWidth: 800),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Center(
+                        child: Image.asset(
+                          'assets/images/DropboxLogo.png',
+                          width: 150,
+                          height: 150,
+                          color: Colors.black,
+                          colorBlendMode: BlendMode.srcIn,
+                          fit: BoxFit.contain,
                         ),
                       ),
-                      const Expanded(child: Center(child: _FormContent())),
-                    ],
-                  ),
+                    ),
+                    const Expanded(child: Center(child: _FormContent())),
+                  ],
                 ),
+              ),
       ),
     );
   }
@@ -57,25 +53,6 @@ class __FormContentState extends State<_FormContent> {
   final TextEditingController passwordController = TextEditingController();
   bool _isPasswordVisible = false;
 
-  String? _validatePassword(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Ingrese su contraseña';
-    }
-    if (value.length < 8) {
-      return 'Debe tener al menos 8 caracteres';
-    }
-    if (!RegExp(r'[A-Z]').hasMatch(value)) {
-      return 'Debe contener al menos una letra mayúscula';
-    }
-    if (!RegExp(r'[0-9]').hasMatch(value)) {
-      return 'Debe contener al menos un número';
-    }
-    if (!RegExp(r'[!@#$%^&*(),.?":{}|<>_/]').hasMatch(value)) {
-      return 'Debe contener al menos un carácter especial (!@#\$%^&*_/)';
-    }
-    return null;
-  }
-
   @override
   Widget build(BuildContext context) {
     final authProvider = Provider.of<AuthProvider>(context);
@@ -91,8 +68,8 @@ class __FormContentState extends State<_FormContent> {
             TextFormField(
               controller: matriculaController,
               decoration: const InputDecoration(
-                labelText: 'Matrícula',
-                hintText: 'Ingrese su matrícula',
+                labelText: 'Nombre de usuario',
+                hintText: 'Ingrese su nombre de usuario',
                 prefixIcon: Icon(Icons.badge_outlined),
                 border: OutlineInputBorder(),
               ),
@@ -100,29 +77,29 @@ class __FormContentState extends State<_FormContent> {
             const SizedBox(height: 16),
             TextFormField(
               controller: passwordController,
-              obscureText: !_isPasswordVisible,
-              validator: _validatePassword,
+              obscureText: !_isPasswordVisible, // Usa la variable para mostrar u ocultar
               decoration: InputDecoration(
                 labelText: 'Contraseña',
                 hintText: 'Ingrese su contraseña',
-                prefixIcon: const Icon(Icons.lock_outline_rounded),
-                border: const OutlineInputBorder(),
+                prefixIcon: Icon(Icons.lock_outline_rounded),
+                border: OutlineInputBorder(),
                 suffixIcon: IconButton(
                   icon: Icon(
-                    _isPasswordVisible
-                        ? Icons.visibility_off
-                        : Icons.visibility,
+                    _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
                   ),
                   onPressed: () {
                     setState(() {
-                      _isPasswordVisible = !_isPasswordVisible;
+                      _isPasswordVisible = !_isPasswordVisible; // Cambia el estado al presionar el ojito
                     });
                   },
                 ),
               ),
             ),
             const SizedBox(height: 16),
-            SizedBox(
+
+            authProvider.isLoading
+            ? const CircularProgressIndicator()
+            :SizedBox(
               width: double.infinity,
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
@@ -138,18 +115,12 @@ class __FormContentState extends State<_FormContent> {
                   ),
                 ),
                 onPressed: () async {
-                  if (!_formKey.currentState!.validate()) {
-                    return;
-                  }
-
                   String nombreCompleto = matriculaController.text;
                   List<String> partes = nombreCompleto.split(".");
 
                   if (partes.length < 2) {
                     ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text("Formato incorrecto de matrícula"),
-                      ),
+                      SnackBar(content: Text("Formato incorrecto de nombre de usuario")),
                     );
                     return;
                   }
@@ -158,27 +129,19 @@ class __FormContentState extends State<_FormContent> {
                   String primerApellido = partes[1];
                   String password = passwordController.text;
 
-                  await authProvider.login(
-                    primerNombre,
-                    primerApellido,
-                    password,
-                  );
+                  await authProvider.login(primerNombre, primerApellido, password);
 
                   if (authProvider.isAuthenticated) {
                     Navigator.pushReplacement(
                       context,
-                      MaterialPageRoute(
-                        builder: (context) => const Bienvenida(),
-                      ),
+                      MaterialPageRoute(builder: (context) => Bienvenida()),
                     );
                   } else {
                     ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text("Error en el inicio de sesión"),
-                      ),
+                      SnackBar(content: Text("Error en el inicio de sesión")),
                     );
                   }
-                },
+                }
               ),
             ),
           ],
